85c6e520f95396a7527048b4a7f1a4df2c9a6ca4
Ernande06 <ernandeneto@gmail.com>
2025-01-28 15:30:20 +0000

    Testanto Novato

--- xadrez.c
+++ xadrez.c
@@ -6,27 +6,88 @@
 
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+    /* Sugestão: Declare variáveis constantes para 
+    representar o número de casas que cada peça pode se mover.*/
+    
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    /* Sugestão: Utilize uma estrutura de repetição
+     para simular a movimentação do Bispo em diagonal.*/
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    /*Sugestão: Utilize uma estrutura de repetição para simular
+     a movimentação da Torre para a direita.*/
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Implementação de Movimentação da Rainha    
+    /* Sugestão: Utilize uma estrutura de repetição
+     para simular a movimentação da Rainha para a esquerda.*/
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
+    /*Nível Aventureiro - Movimentação do Cavalo
+    // Sugestão: Utilize loops aninhados para simular
+     a movimentação do Cavalo em L.*/
+
     // Um loop pode representar a movimentação horizontal e outro vertical.
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
+    /* Sugestão: Implemente a movimentação do 
+    Cavalo utilizando loops com variáveis múltiplas e condições avançadas.*/
     // Inclua o uso de continue e break dentro dos loops.
+
+    int cima, baixo, esq, dir;
+    int opcao, movimento, i;
+
+    do
+    {   
+        printf("\n   *** Vamos    *** \n");
+        printf("--- Jogar Xadrez? ---\n");
+        printf("==============\n");
+        printf("[1] BISPO\n");
+        printf("[2] TORRE\n");
+        printf("[3] RAINHA\n");
+        printf("[0] SAIR\n");
+        printf("==============\n");
+        printf("\nEscolha Movimentar uma Peça: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            printf(" Você escolheu BISPO\n");
+            for ( i = 1; i <= 5; i++)
+            {
+                printf("Direita, Cima\n");
+            }
+            
+            break;
+        
+        case 2:
+            i = 1;
+            printf(" Você escolheu TORRE\n");
+            while (i <= 5)
+            {
+                printf("Direita\n");
+                i++;
+            }
+            
+        break;
+
+        case 3:
+            printf(" Você escolheu RAINHA\n");
+        break;
+        
+        default:
+           printf("Opção Inválida! Tente novamente...\n");
+            break;
+        }
+
+
+
+    } while (opcao !=0);
+    printf("Xadrez FINALIZADO !");
+    
 
     return 0;
 }
